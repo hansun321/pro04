79e9d94e3158f74360808eecc46d3d1c8fe4f41f
hansun321 <hansun321@gmail.com>
2023-10-30 21:37:10 +0900

    Refactor : 비속어

--- src/main/java/kr/ed/haebeop/util/Alert.java
+++ src/main/java/kr/ed/haebeop/util/Alert.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 public class Alert {
-
     public static void alert(HttpServletResponse response, String msg) throws IOException {
         response.setContentType("text/html; charset=UTF-8");
         PrintWriter out = response.getWriter();
